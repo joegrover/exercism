0db02982794188432d8dfc8cb5569d792a9a136c
joegrover <grover@umn.edu>
2020-01-13 22:41:09 -0600

    Auto fresh start: new database, new requests

---
+++ fresh_start.py
@@ -0,0 +1,41 @@
+import random
+import database
+from pymongo import MongoClient
+from pathlib import Path
+import subprocess
+import sys
+import time
+import json
+from mock_requests import mock_nhgis_request, mock_usa_request
+
+CWD = Path.cwd()
+PYTHON = sys.executable
+
+
+def run_client():
+    client = subprocess.Popen(
+        [PYTHON, str(CWD / "extract_requests_client.py"), "-p", "nhgis", "-p", "usa"]
+    )
+    time.sleep(5)
+    return client
+
+
+def run():
+    mclient = MongoClient(host="mongodb://localhost", port=27017)
+    mclient.drop_database("extract_requests")
+    # load database with sample-by-variable connections from csv
+    database.load_usa_sample_variables()
+    # start the client to get extract requests from RabbitMQ
+    client = run_client()
+    # run some mock extract requests
+    for _ in range(random.randint(1, 10)):
+        mock_nhgis_request()
+        mock_usa_request()
+    # run a report:
+    print(json.dumps(database.extract_report()))
+    # keep client running:
+    client.communicate()
+
+
+if __name__ == "__main__":
+    run()
---
+++ mock_requests.py
@@ -0,0 +1,52 @@
+import requests
+import pandas
+import random
+
+sv_df = pandas.read_csv("sample_variables.csv")
+
+
+host = "https://jgrover.hiring.ipums.org"
+usa_endpoint = host + "/publish_usa"
+nhgis_endpoint = host + "/publish_nhgis"
+
+samples = set(sv_df["sample_name"].to_list())
+variables = set(sv_df["variable_mnemonic"].to_list())
+data_tables = set(["1790_cPop.dt1", "1790_cPop.dt8", "1800_cPop.dt1", "1800_cPop.dt9"])
+users = ["glip", "glop", "rocko", "heffer", "Filburt"]
+
+
+def mock_usa_request(repeats=2):
+    k_s = random.randint(1, len(samples))
+    s = random.sample(samples, k=k_s)
+    k_v = random.randint(1, len(variables))
+    v = random.sample(variables, k=k_v)
+    u = random.choice(users)
+    params = {
+        "samples[]": s,
+        "variables[]": v,
+        "user": u,
+        "submit": "Submit",
+    }
+    # XXX Need to repeat here until I figure out the whole client-only
+    # -gets-every-other-message RabbitMQ issue.
+    for _ in range(repeats):
+        resp = requests.get(usa_endpoint, params=params)
+        print(resp)
+    return resp
+
+
+def mock_nhgis_request(repeats=2):
+    k_d = random.randint(1, len(data_tables))
+    d = random.sample(data_tables, k=k_d)
+    u = random.choice(users)
+    params = {
+        "data_tables[]": d,
+        "user": u,
+        "submit": "Submit",
+    }
+    # XXX Need to repeat here until I figure out the whole client-only
+    # -gets-every-other-message RabbitMQ issue.
+    for _ in range(repeats):
+        resp = requests.get(nhgis_endpoint, params=params)
+        print(resp)
+    return resp
